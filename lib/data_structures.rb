50d73c66ac00334394980efa2d44148a630ed239
Ttang8 <thtang28@gmail.com>
2017-04-24 19:52:08 -0700

    Data Structure Exercise

--- lib/data_structures.rb
+++ lib/data_structures.rb
@@ -2,7 +2,7 @@
 
 # Write a method that returns the range of its argument (an array of integers).
 def range(arr)
-  # your code goes here
+  arr.max - arr.min
 end
 
 # Write a method that returns a boolean indicating whether an array is in sorted
@@ -10,7 +10,7 @@ end
 # whether its operands are equal, e.g., 2 == 2 => true, ["cat", "dog"] ==
 # ["dog", "cat"] => false
 def in_order?(arr)
-  # your code goes here
+  arr == arr.sort
 end
 
 
@@ -18,12 +18,12 @@ end
 
 # Write a method that returns the number of vowels in its argument
 def num_vowels(str)
-  # your code goes here
+  str.downcase.count("aeiou")
 end
 
 # Write a method that returns its argument with all its vowels removed.
 def devowel(str)
-  # your code goes here
+  str.delete("aeiouAEIOU")
 end
 
 
@@ -33,26 +33,28 @@ end
 # non-negative integer in descending order and as strings, e.g.,
 # descending_digits(4291) #=> ["9", "4", "2", "1"]
 def descending_digits(int)
-  # your code goes here
+  int.to_s.chars.sort.reverse
 end
 
 # Write a method that returns a boolean indicating whether a string has
 # repeating letters. Capital letters count as repeats of lowercase ones, e.g.,
 # repeating_letters?("Aa") => true
 def repeating_letters?(str)
-  # your code goes here
+  str.downcase.chars.uniq != str.downcase.chars
 end
 
 # Write a method that converts an array of ten integers into a phone number in
 # the format "(123) 456-7890".
 def to_phone_number(arr)
-  # your code goes here
+  "(#{arr[0..2].join}) #{arr[3..5].join}-#{arr[6..10].join}"
 end
 
 # Write a method that returns the range of a string of comma-separated integers,
 # e.g., str_range("4,1,8") #=> 7
 def str_range(str)
-  # your code goes here
+  str.delete!(",")
+  arr = str.chars
+  arr.max.to_i - arr.min.to_i
 end
 
 
@@ -63,5 +65,6 @@ end
 # provided. HINT: use the take(num) and drop(num) methods. You won't need much
 # code, but the solution is tricky!
 def my_rotate(arr, offset=1)
-  # your code goes here
+  rotate = offset % arr.length
+  arr.drop(rotate) + arr.take(rotate)
 end
